8c711fe14699accaf7de02820f68eb2b9872e50e
Akanksha chhabra <92837705+akanksha1814@users.noreply.github.com>
2024-07-05 22:50:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00D7";
+        li.appendChild(span);  
+    }
+    inputBox.value = ''; 
+    savedata();
+}
+
+listContainer.addEventListener('click', function(e) {
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle('checked');
+        savedata();
+    }
+else if(e.target.tagName==="SPAN")
+{
+    e.target.parentElement.remove();
+    savedata();
+}
+},false);
+
+function savedata()
+{
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showList()
+{
+    listContainer.innerHTML= localStorage.getItem("data");
+}
+showList();
